d6602ea0526094602f45d5faf0d8e5d9edc9a109
I-Am-Dench <mrantistupid@gmail.com>
2024-03-09 17:34:20 -0500

    Added account functions

--- server/database/database.py
+++ server/database/database.py
@@ -1,6 +1,6 @@
-from pymongo import MongoClient
+# from pymongo import MongoClient
 
-mongo_client = MongoClient("mongo")
-db = mongo_client["projectDB"]
-all_board = db["boards"]
-chats = db["chat"]
+# mongo_client = MongoClient("mongo")
+# db = mongo_client["projectDB"]
+# all_board = db["boards"]
+# chats = db["chat"]
