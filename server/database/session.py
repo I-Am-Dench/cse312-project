65df2edb898318931bf4e6489858c4a3023aca0b
Eduardo Turcios <eddieturcios@outlook.com>
2024-03-10 18:18:30 -0400

    finished the session

---
+++ server/database/session.py
@@ -0,0 +1,26 @@
+from database import db
+import jwt
+import datetime
+import bcrypt
+
+sessionCollection = db["session"]
+sessionCollection.create_index("expiresIn", expireAfterSeconds=3600)
+
+
+def create_session_token(userID):
+    try:
+        token = {"uid": userID}
+        return jwt.encode(token, "SECRET_KET", algorithm="HS256").encode()
+    except Exception as e:
+        return e
+
+
+def createSession(userID):
+    token = create_session_token(userID)
+    expires = datetime.datetime.utcnow()
+
+    bv = sessionCollection.insert_one(
+        {"token": bcrypt.hashpw(token, bcrypt.gensalt()), "expiresIn": expires}
+    )
+
+    return {"token": token, "Expires": expires}
