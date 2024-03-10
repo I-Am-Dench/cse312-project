17145a929bf6caea6ebfb227498d37c170a6d01c
Eduardo Turcios <67609856+E-Turcios@users.noreply.github.com>
2024-03-10 19:46:35 -0400

    Added database functions for message boards (#12)
    Co-authored-by: Milton Morris <morris56@buffalo.edu>

---
+++ client/src/Components/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    return(
+        <div className='Home'>
+            <div className="background">
+                <h1>JesseFanClub's web project</h1>
+                <image src='./coolguy.jpeg'/>
+            </div>
+        </div>
+    )
+}
+
+export default Home 
