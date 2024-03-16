581be31076015fac0ff5f0384d1df90c00797f19
Eduardo Turcios <eaturcio@buffalo.edu>
2024-03-16 14:50:37 -0500

    added chakraUI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
+import { Flex, Link } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
 import Home from './Components/Home.jsx';
 
 function App() {
-  return <Home />;
+  return (
+    <Flex justifyContent={'center'}>
+      <Link as={RouterLink} to="/">
+        Text
+      </Link>
+    </Flex>
+  );
 }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,8 +5,11 @@ import router from './routes.jsx';
 
 import { RouterProvider } from 'react-router-dom';
 
+import { ChakraProvider } from '@chakra-ui/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </React.StrictMode>
 );
