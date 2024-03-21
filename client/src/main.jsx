bc6b4dbbde58cbb7eef07c81538b45a5263507c1
Eduardo Turcios <eaturcio@buffalo.edu>
2024-03-21 14:55:51 -0500

    added auth provider to main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import router from './routes.jsx';
 
 import { RouterProvider } from 'react-router-dom';
-
+import { AuthProvider } from '../context/AuthContext.jsx';
 import { ChakraProvider } from '@chakra-ui/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
