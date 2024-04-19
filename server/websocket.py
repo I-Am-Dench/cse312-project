1e6b71a5ab1ecc337fea29e5a7d1ae5542aa9697
Kokoxili <shy1491665413@gmail.com>
2024-04-19 17:01:20 -0400

    Updated websocket handlers to use rooms

--- server/websocket.py
+++ server/websocket.py
@@ -1,8 +1,7 @@
-from flask_socketio import SocketIO, emit
-from flask import Flask, request
+from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask import request
 import jwt
 
-# Assuming you have an existing Flask app instance
 from . import create_app
 
 app = create_app()
@@ -16,13 +15,28 @@ def handle_connect():
 def handle_disconnect():
     print('Client disconnected:', request.sid)
 
+@socketio.on('join')
+def on_join(data):
+    username = get_user_from_token(request.cookies.get('AUTH_TOKEN'))
+    room = data['room']
+    join_room(room)
+    emit('status', {'msg': f'{username} has entered the room.'}, room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = get_user_from_token(request.cookies.get('AUTH_TOKEN'))
+    room = data['room']
+    leave_room(room)
+    emit('status', {'msg': f'{username} has left the room.'}, room=room)
+
 @socketio.on('send_message')
 def handle_message(data):
+    room = data['room']
     user = get_user_from_token(request.cookies.get('AUTH_TOKEN'))
     if user:
         message = data['message']
-        print(f'Message received: {message} from {user}')
-        emit('receive_message', {'message': message, 'user': user}, broadcast=True)
+        print(f'Message received in room {room}: {message} from {user}')
+        emit('receive_message', {'message': message, 'user': user}, room=room)
 
 def get_user_from_token(token):
     if token:
