b05067badef76901d71c924bc2a8ed5d361fb3a7
Eduardo Turcios <eddieturcios@outlook.com>
2024-04-20 16:33:03 -0400

    added page to add images to board

--- client/src/page/Board.jsx
+++ client/src/page/Board.jsx
@@ -1,7 +1,25 @@
-import { Button, Flex, Link, FormControl, FormLabel, Input, FormErrorMessage, Container, Box, IconButton } from '@chakra-ui/react';
+import {
+  Button,
+  Flex,
+  Link,
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Container,
+  Box,
+  IconButton,
+} from '@chakra-ui/react';
 import { DeleteIcon } from '@chakra-ui/icons';
-import {Link as RouterLink, useOutletContext, Form, useParams, useNavigate } from 'react-router-dom';
-import { useState, useEffect} from 'react';
+import {
+  Link as RouterLink,
+  useOutletContext,
+  Form,
+  useParams,
+  useNavigate,
+} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import coolguy from '../assets/coolguy.jpg';
 
 function Board() {
   const { boardID } = useParams();
@@ -35,9 +53,9 @@ function Board() {
         navigate('/');
         // window.location.href = "/"
       } else {
-        const json = await response.json()
-        if(json.auth_error) {
-          setUser(null)
+        const json = await response.json();
+        if (json.auth_error) {
+          setUser(null);
         }
       }
     } catch (err) {
@@ -47,41 +65,44 @@ function Board() {
 
   async function deleteComment(commentID) {
     try {
-      const response = await fetch(`/api/boards/${boardID}/comments/${commentID}`, {
-        method: 'DELETE',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          board_id: board.boardID,
-          comment: commentID,
-        }),
-      });
+      const response = await fetch(
+        `/api/boards/${boardID}/comments/${commentID}`,
+        {
+          method: 'DELETE',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            board_id: board.boardID,
+            comment: commentID,
+          }),
+        }
+      );
 
       if (response.ok) {
         const json = await response.json();
         window.location.reload();
       } else {
-        const json = await response.json()
-        if(json.auth_error) {
-          setUser(null)
+        const json = await response.json();
+        if (json.auth_error) {
+          setUser(null);
         }
       }
     } catch (err) {
       console.error(err);
     }
   }
-  
-  async function pullComments(){
+
+  async function pullComments() {
     try {
       const response = await fetch(`/api/boards/${boardID}`, {
         method: 'GET',
-    });
-    if(response.ok){
-      const jsonData = await response.json();
-      setBoard(jsonData.board);
-      setComments(jsonData.comments);
-    }
-  } catch (error) {
-    console.error('Error fetching boards:', error)
+      });
+      if (response.ok) {
+        const jsonData = await response.json();
+        setBoard(jsonData.board);
+        setComments(jsonData.comments);
+      }
+    } catch (error) {
+      console.error('Error fetching boards:', error);
     }
   }
 
@@ -101,20 +122,54 @@ function Board() {
         // navigate('/');
         window.location.reload();
       } else {
-        const json = await response.json()
-        if(json.auth_error) {
-          setUser(null)
+        const json = await response.json();
+        if (json.auth_error) {
+          setUser(null);
         }
       }
     } catch (err) {
       console.error(err);
     }
   }
+  function onImageChange(event) {
+    if (event.target && event.target.files[0]) {
+      formData.append('image_upload', event.target.files[0]);
+    }
+  }
+  async function handleImageSubmit() {
+    try {
+      const response = await fetch(`/api/boards/${boardID}/media"`, {
+        method: 'POST',
+        body: formData,
+      });
 
-    return (
-      <Flex direction={'column'} justifyContent="center" alignItems="center" height="60vh">
-        <Button maxW="150px" alignSelf="center" type="submit" margin={'20px'} onClick={deleteBoard}>Delete Board</Button>
-        {board && (
+      if (response.ok) {
+        window.location.reload();
+      } else {
+        const json = await response.json();
+        setError(json.error);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  return (
+    <Flex
+      direction={'column'}
+      justifyContent="center"
+      alignItems="center"
+      height="60vh"
+    >
+      <Button
+        maxW="150px"
+        alignSelf="center"
+        type="submit"
+        margin={'20px'}
+        onClick={deleteBoard}
+      >
+        Delete Board
+      </Button>
+      {board && (
         <>
           <h1>{board.title}</h1>
           <p>Creator: {board.creatorID}</p>
@@ -122,24 +177,59 @@ function Board() {
       )}
       <br></br>
 
-      <div id="chat-messages" style={{ maxHeight: '90vh', maxWidth: '90vw', height: '90vh', width: '60vw', overflow: 'auto' }}>
-        {comments.map(comment => (      
+      <div
+        id="chat-messages"
+        style={{
+          maxHeight: '90vh',
+          maxWidth: '90vw',
+          height: '90vh',
+          width: '60vw',
+          overflow: 'auto',
+        }}
+      >
+        {comments.map(comment => (
           <div key={comment.id}>
-            <Button onClick={() => deleteComment(comment.id)} style={{ marginRight: '5px' }}>Delete</Button>
-            <b>{comment.CreatorId}</b>: {comment.Content}<br/>
+            <Button
+              onClick={() => deleteComment(comment.id)}
+              style={{ marginRight: '5px' }}
+            >
+              Delete
+            </Button>
+            <b>{comment.CreatorId}</b>: {comment.Content}
+            <br />
           </div>
         ))}
       </div>
 
-        <Form>
-          <FormControl>
-            <FormLabel>Chat</FormLabel>
-            <Input type="content" onChange={e => {handleChangeValue(e, setContent);}} />
-            <Button maxW="150px" alignSelf="center" type="submit" margin={'20px'} onClick={handleSubmit}>Send</Button>
-          </FormControl>
+      <Form>
+        <FormControl>
+          <FormLabel>Chat</FormLabel>
+          <Input
+            type="content"
+            onChange={e => {
+              handleChangeValue(e, setContent);
+            }}
+          />
+          <Button
+            maxW="150px"
+            alignSelf="center"
+            type="submit"
+            margin={'20px'}
+            onClick={handleSubmit}
+          >
+            Send
+          </Button>
+        </FormControl>
+        <FormControl>
+          <FormLabel>Add Image</FormLabel>
+          <Input type="file" name="image_upload" onChange={onImageChange} />
+          <Button maxW="150px" marginTop={'20px'} onClick={handleImageSubmit}>
+            Submit
+          </Button>
+        </FormControl>
       </Form>
-      </Flex>
-    );
-  }
-  
-  export default Board;
+    </Flex>
+  );
+}
+
+export default Board;
